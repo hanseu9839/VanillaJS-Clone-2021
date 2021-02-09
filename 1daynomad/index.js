2b47bbd366e16668fb86b6665a40e545ba173597
둥둥둥오리 <62438578+hanseu9839@users.noreply.github.com>
2021-02-09 20:00:40 +0900

    functionClock-js
    javascript

--- 1daynomad/index.js
+++ 1daynomad/index.js
@@ -14,3 +14,10 @@ const wat = 666;
 
 // Float
 const wat = 55.1;
+
+
+// Array Object camel case 
+const something = "something";
+const daysOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun", something, 54, true, "lalala"];
+
+console.log(daysOfweek);
